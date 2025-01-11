192a316c260ef42d86640b860727ff1526d38d53
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2025-01-10 21:34:54 -0300

    Estruturas de Repetição - For

--- Dia 03/Estruturas de Repetição - For/index.js
+++ Dia 03/Estruturas de Repetição - For/index.js
@@ -1,4 +1,30 @@
 //inicialização; condição; finalização
+
+/*
 for (let indice = 0; indice <= 10; indice++) {
     alert("Indice = " + indice)
+}
+*/
+
+/*
+let indice = 0
+
+for ("Inicializando o for"; indice <= 10; indice++) {
+    alert("Indice = " + indice)
+}
+*/
+
+/*
+let indice = 0
+
+for ("Inicializando o for"; indice <= 10; alert("Finalizando um bloco de repetição")) {
+    alert("Indice = " + indice)
+    indice++
+}
+*/
+
+let nome = "Narcelio"
+
+for (let indice = 0; indice < nome.length; indice++) {
+    console.log(nome[indice])
 }
