bff5a9ebf134a4c2be14287f7946159e1e49ea3a
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-01-14 23:43:43 -0300

    adicionando arquivos JS

---
+++ dia08-revisao/index.js
@@ -0,0 +1,51 @@
+// 1. Declare três variáveis: uma string, um número e um booleano. Exiba os valores no console
+// const nomeP = "João"
+// const numero = 23
+// const estado = true
+// console.log(nome, numero, estado)
+
+
+
+// 2. Crie uma variável que armazene seu nome e outra que armazene sua idade. Exiba uma mensagem como: "Meu nome é [nome] e eu tenho [idade] anos.".
+// let nome = "Narcélio"
+// let idade = 23
+// let apresentacao = "Meu nome é " + nome + " e eu tenho " + idade + " anos."
+// console.log(apresentacao)
+
+
+
+// 3. Crie uma variável para armazenar o valor de um produto e aplique um desconto de 20%. Exiba o preço com desconto no console.
+// let valorProduto = parseFloat(prompt("Qual o valor do produto?"));
+// let precoComDesconto = valorProduto - (valorProduto * 0.2);
+// console.log("Preço com desconto de 20%: R$ " + precoComDesconto.toFixed(2));
+
+
+
+// 4. Faça um programa que calcule a média de três números e exiba o resultado.
+// let n1 = parseFloat(prompt("Digite o primeiro número:"));
+// let n2 = parseFloat(prompt("Digite o segundo número:"));
+// let n3 = parseFloat(prompt("Digite o terceiro número:"));
+// let operacao = (n1+n2+n3)/3
+// alert(`A média dos números ${n1.toFixed(2)}, ${n2.toFixed(2)} e ${n3.toFixed(2)} é: ${operacao.toFixed(2)}`);
+
+
+
+// 5. Verifique se um número armazenado em uma variável é maior que 10 e menor que 20 ao mesmo tempo. Exiba true ou false.
+// let numero = parseFloat(prompt("Digite um número para verificar:"));
+
+// if(numero > 10 && numero < 20) {
+//     alert("O número está entre 10 e 20.");
+// } else {
+//     alert("O número não está entre 10 e 20.");
+// }
+
+
+
+// 6. Crie um programa que verifique se um número é par ou ímpar.
+// let numero = parseFloat(prompt("Digite um número para verificar:"));
+// let resto = numero % 2;
+// if(resto === 0) {
+//     alert(`O número ${numero.toFixed(1)} é PAR`);
+// } else {
+//     alert(`O número ${numero.toFixed(1)} é ÍMPAR`);
+// }
