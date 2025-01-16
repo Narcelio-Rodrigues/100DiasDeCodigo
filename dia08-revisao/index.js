2b0edae088deb86042c13b2be5226c0516c30783
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-01-15 23:21:19 -0300

    Revisão - Dia 08 e 09

--- dia08-revisao/index.js
+++ dia08-revisao/index.js
@@ -1,12 +1,12 @@
-// 1. Declare três variáveis: uma string, um número e um booleano. Exiba os valores no console
-// const nomeP = "João"
+// // 1. Declare três variáveis: uma string, um número e um booleano. Exiba os valores no console
+// const nome = "João"
 // const numero = 23
 // const estado = true
 // console.log(nome, numero, estado)
 
 
 
-// 2. Crie uma variável que armazene seu nome e outra que armazene sua idade. Exiba uma mensagem como: "Meu nome é [nome] e eu tenho [idade] anos.".
+// // 2. Crie uma variável que armazene seu nome e outra que armazene sua idade. Exiba uma mensagem como: "Meu nome é [nome] e eu tenho [idade] anos.".
 // let nome = "Narcélio"
 // let idade = 23
 // let apresentacao = "Meu nome é " + nome + " e eu tenho " + idade + " anos."
@@ -14,14 +14,14 @@
 
 
 
-// 3. Crie uma variável para armazenar o valor de um produto e aplique um desconto de 20%. Exiba o preço com desconto no console.
+// // 3. Crie uma variável para armazenar o valor de um produto e aplique um desconto de 20%. Exiba o preço com desconto no console.
 // let valorProduto = parseFloat(prompt("Qual o valor do produto?"));
 // let precoComDesconto = valorProduto - (valorProduto * 0.2);
 // console.log("Preço com desconto de 20%: R$ " + precoComDesconto.toFixed(2));
 
 
 
-// 4. Faça um programa que calcule a média de três números e exiba o resultado.
+// // 4. Faça um programa que calcule a média de três números e exiba o resultado.
 // let n1 = parseFloat(prompt("Digite o primeiro número:"));
 // let n2 = parseFloat(prompt("Digite o segundo número:"));
 // let n3 = parseFloat(prompt("Digite o terceiro número:"));
@@ -30,22 +30,22 @@
 
 
 
-// 5. Verifique se um número armazenado em uma variável é maior que 10 e menor que 20 ao mesmo tempo. Exiba true ou false.
-// let numero = parseFloat(prompt("Digite um número para verificar:"));
+// // 5. Verifique se um número armazenado em uma variável é maior que 10 e menor que 20 ao mesmo tempo. Exiba true ou false.
+// let numero = parseFloat(prompt("Digite um número para verificar:"))
 
 // if(numero > 10 && numero < 20) {
-//     alert("O número está entre 10 e 20.");
+//     alert("O número está entre 10 e 20.")
 // } else {
-//     alert("O número não está entre 10 e 20.");
+//     alert("O número não está entre 10 e 20.")
 // }
 
 
 
-// 6. Crie um programa que verifique se um número é par ou ímpar.
-// let numero = parseFloat(prompt("Digite um número para verificar:"));
-// let resto = numero % 2;
+// // 6. Crie um programa que verifique se um número é par ou ímpar.
+// let numero = parseFloat(prompt("Digite um número para verificar:"))
+// let resto = numero % 2
 // if(resto === 0) {
-//     alert(`O número ${numero.toFixed(1)} é PAR`);
+//     alert(`O número ${numero.toFixed(1)} é PAR`)
 // } else {
-//     alert(`O número ${numero.toFixed(1)} é ÍMPAR`);
+//     alert(`O número ${numero.toFixed(1)} é ÍMPAR`)
 // }
--- dia09-revisao/index.js
+++ dia09-revisao/index.js
@@ -1,6 +1,6 @@
-// 7. Crie um programa que receba a nota de um aluno (de 0 a 10). Se a nota for maior ou igual a 6, exiba "Aprovado". Caso contrário, exiba "Reprovado".
-
+// // 7. Crie um programa que receba a nota de um aluno (de 0 a 10). Se a nota for maior ou igual a 6, exiba "Aprovado". Caso contrário, exiba "Reprovado".
 // let nota = parseFloat(prompt("Qual a nota do aluno? (de 0 a 10)"))
+
 // if (nota >= 0 && nota <= 10) {
 //     if (nota >= 6) {
 //         alert("Aluno Aprovado!")
@@ -13,8 +13,9 @@
 
 
 
-// 8. Faça um programa que receba a idade de uma pessoa e diga se ela é menor de idade, adulta ou idosa (menor de 18, entre 18 e 60, ou acima de 60).
+// // 8. Faça um programa que receba a idade de uma pessoa e diga se ela é menor de idade, adulta ou idosa (menor de 18, entre 18 e 60, ou acima de 60).
 // let idade = parseFloat(prompt("Digite a sua idade:"))
+
 // if (idade < 18) {
 //     alert("Você é menor de idade!")
 // } else if (idade >= 18 && idade <= 60) {
@@ -25,7 +26,7 @@
 
 
 
-// 9. Escreva um programa que receba um número de 1 a 7 e exiba o dia da semana correspondente (1 para segunda-feira, 7 para domingo).
+// // 9. Escreva um programa que receba um número de 1 a 7 e exiba o dia da semana correspondente (1 para segunda-feira, 7 para domingo).
 // let diaDaSemana = parseFloat(prompt("Digite um número de 1 a 7 para exibir o dia da semana correspondente:"))
 
 // switch (diaDaSemana) {
@@ -56,27 +57,26 @@
 
 
 
-// 10. Crie um programa que receba uma nota de 0 a 10 e exiba o conceito correspondente:
-// 10-9: "A"
-// 8-7: "B"
-// 6-5: "C"
-// 4-0: "D"
+// // 10. Crie um programa que receba uma nota de 0 a 10 e exiba o conceito correspondente:
+// // 10-9: "A"
+// // 8-7: "B"
+// // 6-5: "C"
+// // 4-0: "D"
+// let nota = parseFloat(prompt("Digite sua nota de 0 a 10:"))
 
-let nota = parseFloat(prompt("Digite sua nota de 0 a 10:"))
-
-switch (true) {
-    case nota >= 9 && nota <= 10:
-        alert("A")
-        break;
-    case nota >= 7 && nota < 9:
-        alert("B")
-        break;
-    case nota >= 5 && nota < 7:
-        alert("C")
-        break;
-    case nota >= 0 && nota < 5:
-        alert("D")
-        break
-    default:
-        alert("Digite uma nota válida entre 0 e 10!")
-}
+// switch (true) {
+//     case nota >= 9 && nota <= 10:
+//         alert("A")
+//         break
+//     case nota >= 7 && nota < 9:
+//         alert("B")
+//         break
+//     case nota >= 5 && nota < 7:
+//         alert("C")
+//         break
+//     case nota >= 0 && nota < 5:
+//         alert("D")
+//         break
+//     default:
+//         alert("Digite uma nota válida entre 0 e 10!")
+// }
