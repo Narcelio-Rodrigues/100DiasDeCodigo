f22361cab5713a0c582d6070ec826484122f344e
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-01-16 22:35:15 -0300

    Revisão - Dia 10

---
+++ dia10-revisao/index.js
@@ -0,0 +1,23 @@
+// 11. Faça um programa que exiba todos os números de 1 a 10 usando while.
+// let numero = 1
+
+// while (numero <= 10) {
+//     console.log(numero)
+//     numero++
+// }
+
+
+
+// 12. Crie um programa que receba números do usuário até que ele digite um número negativo. Exiba a soma de todos os números digitados.
+// let soma = 0
+// let numero
+
+// do {
+//     numero = parseFloat(prompt("Digite um número (ou um número negativo para sair):"))
+
+//     if (numero >= 0) { 
+//         soma += numero
+//     }
+// } while (numero >= 0)
+
+// alert("A soma de todos os números digitados é: " + soma)
